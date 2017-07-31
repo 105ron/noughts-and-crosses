75993643935e74e05f55504d5b6f1e62ba3e2b08
Rhys <rbrooker105@gmail.com>
2017-07-31 21:34:32 +1000

    add classes for noughts and crosses

--- scripts/game.js
+++ scripts/game.js
@@ -2,12 +2,12 @@
 const rows = 3;
 const columns = 3;
 const gameSquare = document.getElementById('game-square');
-
+let gameState = Array(...Array(rows)).map(() => Array(columns));
 
 const gameGrid = function gameGrid() {
   const grid = Array.apply(null, Array(columns)).map( (y, yIndex) => { 
     let row = Array.apply(null, Array(rows)).map( (x, xIndex) => {
-      return `<button class="game-squares ${ xIndex }-${ yIndex }"></button>`;
+      return `<button class="game-squares pos${ xIndex }-${ yIndex }"></button>`;
     });
     return `${ row.join('') }`;
   });
