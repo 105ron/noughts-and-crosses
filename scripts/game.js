0af1871a8a8648e730992dc1d8b0e2164aab8b33
Rhys <rbrooker105@gmail.com>
2017-08-02 21:41:03 +1000

    add functions to check for draw and winner

--- scripts/game.js
+++ scripts/game.js
@@ -6,8 +6,8 @@ const gameSquares = document.getElementsByClassName('game-squares');
 let gameState = Array(...Array(rows)).map(() => Array(columns));
 
 const gameGrid = () => {
-  const grid = Array.apply(null, Array(columns)).map( (y, yIndex) => { 
-    let row = Array.apply(null, Array(rows)).map( (x, xIndex) => {
+  const grid = Array.apply(null, Array(columns)).map( (x, xIndex) => { 
+    let row = Array.apply(null, Array(rows)).map( (y, yIndex) => {
       return `<button class="game-squares pos${ xIndex }-${ yIndex }" 
               data-x="${ xIndex }" data-y="${ yIndex }"></button>`;
     });
@@ -27,13 +27,51 @@ const updateBoard = (x, y, player) => {
   clickedSquare.insertAdjacentHTML('afterbegin', boardTags[player]);
 };
 
+const isLegalMove = (x, y) => {
+  return (gameState[x][y]) ? false : true;
+};
+
+const gameLines = (gameArray) => {
+  //rows, columns and diagonals of the game board
+  const gameLines = [
+    [...gameArray[0]], //rows
+    [...gameArray[1]],
+    [...gameArray[2]],
+    [gameArray[0][0], gameArray[1][0], gameArray[2][0]], //columns
+    [gameArray[0][1], gameArray[1][1], gameArray[2][1]],
+    [gameArray[0][2], gameArray[1][2], gameArray[2][2]],
+    [gameArray[0][0], gameArray[1][1], gameArray[2][2]], //diagonals
+    [gameArray[0][2], gameArray[1][1], gameArray[2][0]]
+  ];
+  return gameLines;
+}
+
+const isWinner = (gameArray, playerToken) => {
+  let result = false;
+  gameLines(gameArray).forEach(gameLine => {
+    if (gameLine.every(gameSquare => (gameSquare === playerToken))) result = true;
+  });
+  return result;
+};
+
+const isDraw = (gameArray) => {
+  const flattenedGame = [].concat(...gameArray)
+  console.log(flattenedGame);
+  return !flattenedGame.includes(undefined);
+};
+
+
 const gameClick = function gameClick() {
   const xPosition = parseInt(this.getAttribute("data-x"));
   const yPosition = parseInt(this.getAttribute("data-y"));
   let playerTurnId = document.getElementById('player-turn');
   const playerTurn = playerTurnId.innerText;
-  updateBoard(xPosition,yPosition, playerTurn);
-  playerTurnId.innerText = (playerTurn == 'X') ? 'O' : 'X';
+  if (isLegalMove(xPosition, yPosition)) {
+    updateBoard(xPosition,yPosition, playerTurn);
+    gameState[xPosition][yPosition] = playerTurn;
+    //(isWinner(gameState, playerTurn));
+    playerTurnId.innerText = (playerTurn == 'X') ? 'O' : 'X';
+  };
 };
 
 Array.from(gameSquares).forEach(function(element) {
